ae6c31cc5258180a284b02c954743cddbd3ac004
lhl <lhl@randomfoo.net>
2019-09-22 17:39:48 -0700

    Get old comments from pushshift

---
+++ reddit/get-old-comments.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import json
+import os
+from   pprint import pprint
+import psaw
+import sys
+import time
+
+'''
+https://pushshift.io/what-is-pushshift-io/
+https://pushshift.io/api-parameters/
+https://files.pushshift.io/reddit/
+
+https://github.com/dmarx/psaw
+
+/reddit/comment/search?
+
+sort=asc
+sort_type=[created_utc]
+before=[epoch]
+author=randomfoo2
+
+ERROR comment dhum8ac...
+'''
+
+api = psaw.PushshiftAPI()
+# gen = api.search_comments(author='randomfoo2', before='1495394156')
+# pprint(next(gen))
+# pprint(len(list(gen)))
+gen = api.search_comments(author='randomfoo2', before='1446592512')
+pprint(len(list(gen)))
